4cc4a0d4e04e78e4fd543fbacf0c9b2da82cd917
UStuej <45504535+UStuej@users.noreply.github.com>
2019-01-02 13:40:06 -0600

    Add files via upload

--- hddledsf.py
+++ hddledsf.py
@@ -2,14 +2,14 @@ from os import rename
 from time import sleep
 
 # Constants
-MSG_PATH = "Path to .txt file (from this program) containing message to transmit"
-FOLDER_A_PATH = "(Complete) Path to folder A"
-FOLDER_B_PATH = "(Complete) Path to folder B"
-MOVE_DATA_PATH = "Path to data to move (from within folder A at FOLDER_A_PATH)"
-BIT_SLEEP_TIME = X
-ONE_SLEEP_TIME = X
-SECURITY_BIT_REPEATS = X
-SECURITY_BIT_SLEEP_TIME = X
+MSG_PATH = "hddledsfmsg.txt"
+FOLDER_A_PATH = "C:/Users/Ben Alford/Desktop/hddledsfa"
+FOLDER_B_PATH = "C:/Users/Ben Alford/Desktop/hddledsfb"
+MOVE_DATA_PATH = "hddledsfmvdat"
+BIT_SLEEP_TIME = 6 
+ONE_SLEEP_TIME = 2
+SECURITY_BIT_REPEATS = 3
+SECURITY_BIT_SLEEP_TIME = 4
 
 f = open(MSG_PATH,"r")
 msg = list(f.read())
@@ -40,6 +40,7 @@ try:
                 m2state = "a"
             if bit == "1":
                 sleep(ONE_SLEEP_TIME)
+                print("========================================")
                 move(m2state)
                 if m2state == "a":
                     m2state = "b"
